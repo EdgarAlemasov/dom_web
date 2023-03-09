df1a5ab6fa8fdba26f4c0624b9f166f333f8f0a6
Edgar Alemasov <edgar.alemasov@gmail.com>
2023-03-09 22:03:14 +0500

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+![CI](https://github.com/EdgarAlemasov/dom_web/actions/workflows/web.yml/badge.svg)
+[![Build status](https://ci.appveyor.com/api/projects/status/4ilqflqa5soity5v?svg=true)](https://ci.appveyor.com/project/EdgarAlemasov/dom-web)
---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+import goblinImg from './../img/goblin.png'
+
+class Game {
+  constructor() {
+    this.previosCord = 0;
+    this.counter = 0;
+    this.field = document.querySelectorAll(".game_field");
+  }
+
+  activeImg() {
+    const img = document.createElement("img");
+    img.src = goblinImg;
+    return img;
+  }
+
+  dellImg() {
+    const img = document.querySelector("img");
+    if (img) {
+      img.remove();
+    }
+  }
+
+  randomNumber(maximum) {
+    return Math.floor(Math.random() * maximum);
+  }
+
+  randomInsert() {
+    const cordField = this.randomNumber(this.field[0].children.length);
+    if (cordField == this.previosCord) {
+      this.randomInsert();
+    } else {
+      this.previosCord = cordField;
+      this.field[0].children[cordField].appendChild(this.activeImg());
+    }
+  }
+
+  main() {
+    this.dellImg();
+    this.randomInsert();
+    this.counter += 1;
+  }
+}
+
+const newGame = new Game();
+
+setInterval(() => newGame.main(), 1000);
